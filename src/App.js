d397a341ac70b625a07d85d22655341fca8737ce
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-27 22:51:14 +0530

    added firebase api for the user login and signup authentication

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
          <LogIn /> 
     </div>
 </ThemeProvider>
-);
+  );
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,67 +1,146 @@
+import { useState,useRef } from "react";
 import { Container, Modal, Button, Form,Stack, Spinner } from "react-bootstrap";
 import { Row, Col } from "react-bootstrap";
 const LogIn = () => {
 
+    const [isLogIn, setIsLogIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isExisting, setIsExisting] = useState(false);
+    
+    const enteredEmail = useRef(null);
+    const enteredPassword = useRef(null);
 
+const submitFormHandler = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const email = enteredEmail.current.value;
+    const password = enteredPassword.current.value;
 
-    return(
+    let url = isLogIn
+      ? "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBj8h0OYKwQY8xN_uzWw2Crp801EclmMJg"
+      : "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBj8h0OYKwQY8xN_uzWw2Crp801EclmMJg";
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }),
+    })
+      .then((response) => {
+        setIsLoading(false);
+        return response.json();
+      })
+      .then((data) => {
+        if (data.hasOwnProperty("error")) {
+          alert(data.error.message);
+          enteredEmail.current.value = "";
+          enteredPassword.current.value = "";
+        } else {
+          enteredEmail.current.value = "";
+          enteredPassword.current.value = "";
+          localStorage.setItem("token", data.idToken);
+          localStorage.setItem("email", email);
+        //   navigate("/Home");
+          if (isLogIn) {
+            alert("Log in successful");
+          } else {
+            alert(
+              "Your account is created successfully. Now you can log in using your credentials"
+            );
+          }
+        }
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        alert("An error occurred. Please try again later.");
+        enteredEmail.current.value = "";
+        enteredPassword.current.value = "";
+      });
+    };
+    
+    const existingAccountHandler = () => {
+    setIsExisting((prevState) => !prevState);
+    setIsLogIn((prevState) => !prevState);
+    };
+      
+    const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+  };
+
+    return (
         <div>
             <Container>
-                <Row  className="justify-content-center">
-                    <Col xl={4} lg={4} sm={12} xs={12} >
-                <div className="modal show" style={{position:"initial",display:"block"}}>
-                    <Modal.Dialog >
-                                <Modal.Header style={{fontWeight: "bolder", fontSize: "2rem" }}
-                                className="m-auto mb-3">
-                            SignUp
-                        </Modal.Header>
-                        <Modal.Body>
-                            <Form>
-                                <Form.Group as={Col} className="mb-3">
-                                    <Form.Label style={{fontWeight:"bold",fontSize:"1.2rem"}}>
-                                        Email Id  
-                                    </Form.Label>
-                                    <Form.Control type="email"/>
-                                </Form.Group>
-                                <Form.Group as={Col} className="mb-3">
-                                <Form.Label style={{fontWeight:"bold",fontSize:"1.2rem"}}>
-                                        Password 
-                                    </Form.Label>
-                                    <Form.Control type="password"/>
-                                </Form.Group>
-                                  <Form.Group as={Col} className="mb-3">
-                                <Form.Label style={{fontWeight:"bold",fontSize:"1.2rem"}}>
-                                        Confirm Password  
-                                    </Form.Label>
-                                    <Form.Control type="password"/>
-                                </Form.Group>
-                            </Form>
-                            <Stack>
-                                <div className="d-grid">
-                                <Button className="mb-1" style={{fontWeight:"bold",fontSize:"1.1rem"}}>
-                                    Sign up
-                                </Button>
-                                </div>
-                                <div className="m-2">
-      <Spinner animation="grow" variant="danger" />
-      <Spinner animation="grow" variant="warning" />
-      <Spinner animation="grow" variant="info" />
-                              </div>
-                            </Stack>
-                            <Container>
-                                <div className="m-auto text-primary">Forget Password ?</div>
-                            </Container>
-                        </Modal.Body>
-                        <Modal.Footer className="m-auto m-2 text-primary">
-                            Log into existing account.
-                        </Modal.Footer>
-                    </Modal.Dialog>
-                </div>
+                <Row style={containerStyle}>
+                    <Col xl={4} lg={4} sm={12} xs={12}>
+                        <div className="modal show" style={{ position: "initial", display: "block" }}>
+                            <Modal.Dialog>
+                                <Modal.Header style={{ fontWeight: "bolder", fontSize: "2rem" }}
+                                    className="m-auto mb-3">
+                                    {isExisting || isLogIn ? <div>SignUp</div> : <div>LogIn</div>}
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form onSubmit={submitFormHandler}>
+                                        <Form.Group as={Col} className="mb-3">
+                                            <Form.Label style={{ fontWeight: "bold", fontSize: "1.2rem" }}
+                                            >
+                                                Email Id
+                                            </Form.Label>
+                                            <Form.Control type="email" ref={enteredEmail} />
+                                        </Form.Group>
+                                        <Form.Group as={Col} className="mb-3">
+                                            <Form.Label style={{ fontWeight: "bold", fontSize: "1.2rem" }}
+                                            >
+                                                Password
+                                            </Form.Label>
+                                            <Form.Control type="password" ref={enteredPassword} />
+                                        </Form.Group>
+                                        <Form.Group as={Col} className="mb-3">
+                                            <Form.Label style={{ fontWeight: "bold", fontSize: "1.2rem" }}
+                                          >
+                                                Confirm Password
+                                            </Form.Label>
+                                            <Form.Control type="password" />
+                                        </Form.Group>
+                                    </Form>
+                                    <Stack>
+                                        <div className="d-grid">
+                                            <Button className="mb-1" style={{ fontWeight: "bold", fontSize: "1.1rem" }}
+                                            type="submit">
+                                                {isLogIn || isExisting ? <div >SignUp</div> : <div >LogIn</div>}
+                                            </Button>
+                                        </div>
+                                        <div className="m-2">
+                                            {isLoading && <div>
+                                            <Spinner animation="grow" variant="danger" />
+                                            <Spinner animation="grow" variant="warning" />
+                                            <Spinner animation="grow" variant="info" /></div>}
+                                        </div>
+                                    </Stack>
+                                    <Container>
+                                        <div className="m-auto text-primary">Forget Password ?</div>
+                                    </Container>
+                                </Modal.Body>
+                                <Modal.Footer className="m-auto m-2 text-primary">
+                                    { isExisting ? <div onClick={existingAccountHandler}
+                                    className="border-bottom border-primary mb-2">Log into existing account.</div>
+                                        : <div onClick={existingAccountHandler}
+                                        className="border-bottom border-primary mb-2">New User ? Create a new account.</div>}
+                                </Modal.Footer>
+                            </Modal.Dialog>
+                        </div>
                     </Col>
                 </Row>
 
             </Container>
         </div>
-    )
+    );
 }
 export default LogIn;
