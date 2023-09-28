7612e4f1d1e819c2f2534abb402de18085873c90
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-28 15:47:35 +0530

    corrected the code of login component,corrected the conditional rendering of the texts,also adjusted the size of image

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,43 @@
 import { useState,useRef } from "react";
-import { Container, Modal, Button, Form,Stack, Spinner,Image } from "react-bootstrap";
+import { Container, Modal, Button, Form,Stack, Spinner,Image, Alert } from "react-bootstrap";
 import { Row, Col } from "react-bootstrap";
 import loginimg from "../assets/login.jpg";
+import { FaLock } from "react-icons/fa";
+import { FaKey } from "react-icons/fa";
+
+
 const LogIn = () => {
 
     const [isLogIn, setIsLogIn] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const [isExisting, setIsExisting] = useState(false);
+  const [isExisting, setIsExisting] = useState(false);
+  const [matchPassword, setMatchPassword] = useState(true);
     
     const enteredEmail = useRef(null);
     const enteredPassword = useRef(null);
+    const confirmPassword = useRef(null);
+  
 
+  console.log("form submitted 1"); 
+  
 const submitFormHandler = (e) => {
-    e.preventDefault();
+  e.preventDefault();
+  console.log("form submitted 2");
     setIsLoading(true);
     const email = enteredEmail.current.value;
     const password = enteredPassword.current.value;
+     
+  if (!isLogIn) { 
+    const confirmPasswordValue = confirmPassword.current.value;
+    if (password !== confirmPasswordValue) {
+      setMatchPassword(false);
+      setIsLoading(false);
+      return;
+    } else {
+      setMatchPassword(true);
+    }
+  }
+
 
     let url = isLogIn
       ? "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBj8h0OYKwQY8xN_uzWw2Crp801EclmMJg"
@@ -41,6 +63,7 @@ const submitFormHandler = (e) => {
           alert(data.error.message);
           enteredEmail.current.value = "";
           enteredPassword.current.value = "";
+          confirmPassword.current.value = "";
         } else {
           enteredEmail.current.value = "";
           enteredPassword.current.value = "";
@@ -61,6 +84,7 @@ const submitFormHandler = (e) => {
         alert("An error occurred. Please try again later.");
         enteredEmail.current.value = "";
         enteredPassword.current.value = "";
+        confirmPassword.current.value = "";
       });
     };
     
@@ -80,52 +104,54 @@ const submitFormHandler = (e) => {
     return (
         <div style={{overflowX:"hidden",minHeight:"100vh"}}>
             <Container>
-
+          {!matchPassword && <Alert variant="danger" dismissible>
+            <div>Your confirm password did not match!</div>
+          </Alert>}
                 <Row style={containerStyle}>
-                    <Col xl={8} lg={8} sm={12} xs={12}>
+                    <Col xl={6} lg={6} sm={12} xs={12}>
                     <Image src={loginimg}
                             alt="Image alt text"
                            width={isSmaller ? "350px" : "600px" } 
-                           height={isSmaller ? "350px" : "600px" } 
+                           height={isSmaller ? "350px" : "550px" } 
                     className="rounded"/>
                     </Col>
-                    <Col xl={4} lg={4} sm={4} xs={12}>
-                        <div className="modal show" style={{ position: "initial", display: "block" }}>
+            <Col xl={6} lg={6} sm={12} xs={12}>
+                        <div className="modal show" style={{ position: "initial", display: "block",width: isSmaller ? "auto" : "380px" }}>
                             <Modal.Dialog>
-                                <Modal.Header style={{ fontWeight: "bolder", fontSize: "2rem" }}
-                                    className="m-auto mb-3">
-                                    {isExisting || isLogIn ? <div>SignUp</div> : <div>LogIn</div>}
+                                <Modal.Header style={{ fontWeight: "bolder" }}
+                                    className="m-auto mb-2">
+                                    {isExisting || isLogIn ? <div><h1>LogIn<span className="m-2"><FaKey size={24}/></span></h1></div> : <div><h1>SignUp<span className="m-2"><FaLock size={24}/></span></h1></div>}
                                 </Modal.Header>
                                 <Modal.Body>
-                                    <Form onSubmit={submitFormHandler}>
-                                        <Form.Group as={Col} className="mb-3">
-                                            <Form.Label style={{ fontWeight: "bold", fontSize: "1.2rem" }}
-                                            >
-                                                Email Id
-                                            </Form.Label>
-                                            <Form.Control type="email" ref={enteredEmail} />
-                                        </Form.Group>
-                                        <Form.Group as={Col} className="mb-3">
-                                            <Form.Label style={{ fontWeight: "bold", fontSize: "1.2rem" }}
-                                            >
-                                                Password
-                                            </Form.Label>
-                                            <Form.Control type="password" ref={enteredPassword} />
-                                        </Form.Group>
-                                        <Form.Group as={Col} className="mb-3">
-                                            <Form.Label style={{ fontWeight: "bold", fontSize: "1.2rem" }}
-                                          >
-                                                Confirm Password
-                                            </Form.Label>
-                                            <Form.Control type="password" />
-                                        </Form.Group>
-                                    </Form>
+                                   <Form onSubmit={submitFormHandler}>
+                                  <Form.Group as={Col} className="mb-2">
+                                <Form.Label style={{ fontWeight: "bold" }}>
+                               Email Id
+                              </Form.Label>
+                             <Form.Control type="email" ref={enteredEmail} />
+                           </Form.Group>
+                         <Form.Group as={Col} className="mb-2">
+                                  <Form.Label style={{ fontWeight: "bold" }}>
+                                Password
+                            </Form.Label>
+                            <Form.Control type="password" ref={enteredPassword} />
+                          </Form.Group>
+                      {!isLogIn && <Form.Group as={Col} className="mb-2">
+                        <Form.Label style={{ fontWeight: "bold" }}>
+                          Confirm Password
+                        </Form.Label>
+                        <Form.Control type="password" ref={confirmPassword} />
+                      </Form.Group>}
                                     <Stack>
                                         <div className="d-grid">
-                                            <Button className="mb-1" style={{ fontWeight: "bold", fontSize: "1.1rem" }}
-                                            type="submit">
-                                                {isLogIn || isExisting ? <div >SignUp</div> : <div >LogIn</div>}
-                                            </Button>
+                                           <Button
+                                          className="mb-2"
+                                         style={{ fontWeight: "bold", fontSize: "1.1rem" }}
+                                        type="submit"
+                            onClick={() => console.log("Button clicked")} // Add this for debugging
+                          >
+                            {isLogIn || isExisting ? <div>LogIn</div> : <div>SignUp</div>}
+                            </Button>
                                         </div>
                                         <div className="m-2">
                                             {isLoading && <div>
@@ -134,15 +160,16 @@ const submitFormHandler = (e) => {
                                             <Spinner animation="grow" variant="info" /></div>}
                                         </div>
                                     </Stack>
+                                  </Form>
                                     <Container>
                                         <div className="m-auto text-primary">Forget Password ?</div>
                                     </Container>
                                 </Modal.Body>
                                 <Modal.Footer className="m-auto m-2 text-primary">
                                     { isExisting ? <div onClick={existingAccountHandler}
-                                    className="border-bottom border-primary mb-2">Log into existing account.</div>
+                                    className="border-bottom border-primary mb-2">New User ? Create a new account.</div>
                                         : <div onClick={existingAccountHandler}
-                                        className="border-bottom border-primary mb-2">New User ? Create a new account.</div>}
+                                        className="border-bottom border-primary mb-2">Log into existing account.</div>}
                                 </Modal.Footer>
                             </Modal.Dialog>
                         </div>
