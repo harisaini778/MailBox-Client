7cd6f57af90fa13d4f2dcf5d147ffbb635ece13d
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-10-05 00:19:19 +0530

    added a new component in the project wgich help the user to read the entire message also added it's stylesheet in the code

---
+++ src/components/InboxMessageDetails.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { FaArrowLeft } from "react-icons/fa";
+import { useMessageContext } from "./MessageContextProvider";
+import "./InboxMessageDetail.css"; // Import your custom CSS file for styling
+
+const InboxMessageDetails = ({ messageId }) => {
+  const ctx = useMessageContext();
+  const message = ctx.messages.find((msg) => msg.id === messageId);
+
+  if (!message) {
+    return null; // Message not found, handle this case as needed
+  }
+
+  const handleBackClick = () => {
+    ctx.closeMessageDetail(); // Implement a function in your context to close the message detail view
+  };
+
+  return (
+    <div className="message-detail">
+      <Container fluid>
+        <Row>
+          <Col>
+            <Button
+              variant="light"
+              className="back-button"
+              onClick={handleBackClick}
+            >
+              <FaArrowLeft />
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>{message.subject}</h3>
+            <p className="message-sender">
+              From: {message.sender} -{" "}
+              {new Date(message.date).toLocaleString()}
+            </p>
+            <div className="message-body">{message.body}</div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default InboxMessageDetails;
