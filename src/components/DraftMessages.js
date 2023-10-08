26d4d1cdaf3c02ee4c5b94f1e6f7e51c50405d34
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-10-08 19:48:33 +0530

    added a new draftbox component in the project, a user can easily click on the draft to read the draft box messages

--- src/components/DraftMessages.js
+++ src/components/DraftMessages.js
@@ -2,12 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { Container, ListGroup, Row, Col, Stack } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux'; 
 import { fetchDraftMessages } from '../store/dataStore'; 
+import { toggleMessageDetail } from '../store/dataStore';
+import DraftboxMessages  from './DraftBoxMessages';
+import "./SentMessages.css";
 
 const DraftMessages = () => {
   const [isSmaller, setIsSmaller] = useState(window.innerWidth <= 576);
+  const [selectedMessageId, setSelectedMessageId] = useState(null);
+  const isMessageDetailOpen = useSelector((state) => state.dataStore.isMessageDetailOpen);
   const draftMessages = useSelector((state) => state.dataStore.draftMessages); 
   const draft = Object.values(draftMessages);
   const dispatch = useDispatch(); 
+
+    const handleListItemClick = (messageId) => {
+    setSelectedMessageId(messageId);
+    dispatch(toggleMessageDetail());
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -34,37 +44,41 @@ const DraftMessages = () => {
 
   return (
     <Container className="mt-3">
-      <ListGroup>
+      
+       {selectedMessageId && isMessageDetailOpen && (
+        <DraftboxMessages messageId={selectedMessageId} draft={draft} />
+      )}
+      {!isMessageDetailOpen && <ListGroup>
         {draft.map((message) => (
-          <ListGroup.Item
-            key={message.id}
-            className={message.unread ? 'unread' : ''}
-          >
-            <Row>
-              <Col>
-                <input type="checkbox" />
-              </Col>
-              <Col style={{ fontWeight: 'bold' }} className="truncate-text-sent">
-                To: {message.to}
-              </Col>
-              {!isSmaller && (
-                <Col>
-                  <Stack direction="horizontal" gap="1">
-                    <span>{message.ccBccOption} : </span>
-                    <span>{message.ccBccValue}</span>
-                  </Stack>
-                </Col>
-              )}
-              <Col style={{ fontWeight: 'bold' }} className="truncate-text-sent">
-                {message.subject}
-              </Col>
-              <Col className="truncate-text-sent">
-                {stripHtmlTags(message.message)}
-              </Col>
-            </Row>
-          </ListGroup.Item>
+    <ListGroup.Item
+    key={message.id}
+  onClick={() => handleListItemClick(message.id)}
+>
+  <Row className=''>
+    <Col>
+      <input type="checkbox" />
+    </Col>
+    <Col style={{ fontWeight: 'bold' }} className="truncate-text-sent">
+      To: {message.to}
+    </Col>
+    {!isSmaller && (
+      <Col>
+        <Stack direction="horizontal" gap="1">
+          <span>{message.ccBccOption} : </span>
+          <span>{message.ccBccValue}</span>
+        </Stack>
+      </Col>
+    )}
+    <Col style={{ fontWeight: 'bold' }} className="truncate-text-sent">
+      {message.subject}
+    </Col>
+    <Col className="truncate-text-sent">
+      {stripHtmlTags(message.message)}
+    </Col>
+  </Row>
+</ListGroup.Item>
         ))}
-      </ListGroup>
+      </ListGroup>}
     </Container>
   );
 };
